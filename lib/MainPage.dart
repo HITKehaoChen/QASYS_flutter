fe898fd0dd4215ed6277ca0c098ce7e9159882b4
littlecoke <tgyzckh@hotmail.com>
2018-08-26 03:03:15 +0800

    再不写就凉了

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -36,6 +36,44 @@ class MainPageState extends State<MainPageWidget>{
     }
   }
 
+  var _bodys;
+  void initData(){
+    _bodys = [
+      new AskPage(),
+      new HistoryPage(),
+      new AboutPage(),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar:  new AppBar(
+        title: new Text("Home"),
+
+      ),
+      body: _bodys[_tabIndex],
+      bottomNavigationBar: new BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          new BottomNavigationBarItem(
+              icon: Icon(Icons.question_answer), title: getTabTitle(0)),
+          new BottomNavigationBarItem(
+              icon: Icon(Icons.history), title: getTabTitle(1)),
+          new BottomNavigationBarItem(
+              icon: Icon(Icons.info_outline), title: getTabTitle(2)),
+
+        ],
+        currentIndex:  _tabIndex,
+        onTap: (index){
+          setState(() {
+            _tabIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
 
 
 }
